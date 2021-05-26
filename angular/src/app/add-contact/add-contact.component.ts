87d880352b70d4edd7a12b02fc1f749e761c51c4
Lafaa <troise.davide@libero.it>
2021-05-26 07:52:36 +0200

    up

--- angular/src/app/add-contact/add-contact.component.ts
+++ angular/src/app/add-contact/add-contact.component.ts
@@ -5,7 +5,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { catchError, tap } from 'rxjs/operators';
 import { AppStateService } from '../services/app-state.service';
 import { ZendeskService } from '../services/zendesk.service';
-// import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-add-contact',
@@ -41,15 +40,18 @@ export class AddContactComponent {
     }
     const data = { ...this.createForm.value };
     data.subdomain = this.context.account.subdomain;
-    // data.iv = this.makeid(16);
-    // data.apiKEY = this.aesEncrypt(data.apiKEY, data.iv);
-
-    // return;
 
     this.appStateService.startLoading('Sending info');
 
     this.http.post('https://hook.integromat.com/ofr0eejkr89t1x5j0akpv87uiduotk8i', data, { headers: this.headers, responseType: 'text' })
       .pipe(
+        tap(() => {
+          this.createForm.reset();
+          this.createForm.get('username')!.clearValidators();
+          this.createForm.get('apiKEY')!.clearValidators();
+          this.createForm.get('username')!.updateValueAndValidity();
+          this.createForm.get('apiKEY')!.updateValueAndValidity();
+        }),
         tap(() => this.zendeskService.showNotification('Account created or updated correctly!')),
         tap(() => this.appStateService.stopLoading()),
         catchError(
@@ -63,33 +65,5 @@ export class AddContactComponent {
       ).subscribe();
   }
 
-  // private aesEncrypt(data: string, iv: string) {
-  //   const key = '10izM1LG7AVx8CF6QfvgVmMmCYJi8xdd';
-  //   const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
-  //     iv: CryptoJS.enc.Utf8.parse(iv)
-  //   });
-
-  //   return cipher.toString();
-  // }
-
-  // private aesDecrypt(data: string, iv: string) {
-  //   const key = '10izM1LG7AVx8CF6QfvgVmMmCYJi8xdd';
-  //   const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
-  //     iv: CryptoJS.enc.Utf8.parse(iv)
-  //   });
-
-  //   return cipher.toString(CryptoJS.enc.Utf8);
-  // }
-
-  // private makeid(length: number) {
-  //   const result = [];
-  //   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  //   const charactersLength = characters.length;
-  //   for (let i = 0; i < length; i++) {
-  //     result.push(characters.charAt(Math.floor(Math.random() *
-  //       charactersLength)));
-  //   }
-  //   return result.join('');
-  // }
 }
 
